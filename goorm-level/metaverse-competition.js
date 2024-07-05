4eb18fd13e7e545013449e74a5c7cac445b70489
KimGyeongyeon <kgy556@naver.com>
2024-07-05 15:28:08 +0900

    feat: 다시 brute-force 방식으로 되돌

--- goorm-level/metaverse-competition.js
+++ goorm-level/metaverse-competition.js
@@ -15,123 +15,73 @@
 import * as readline from 'node:readline/promises';
 
 // const readline = require('readline');
-const METAVERSE_SIZE = 1000;
+const metaverseSize = 16
+
 
 (async () => {
-  let rl = readline.createInterface({ input: process.stdin });
-  let n = undefined;
-  let k = undefined;
-  let spaces = [];
-  for await (const line of rl) {
-    if (n == undefined && k == undefined) {
-      [n, k] = line.split(' ');
-      if (n == undefined || k == undefined) {
-        throw new Error('인풋 받는거 잘못됨');
-      }
-    } else {
-      spaces.push(line.split(' '));
-      if (spaces.length >= n) {
-        rl.close();
-        break;
-      }
-    }
-  }
-
-  solution(n, k, spaces);
-
-  process.exit();
+	let rl = readline.createInterface({ input: process.stdin });
+	let n = undefined;
+	let k = undefined;
+	let spaces = [];
+	for await (const line of rl) {
+		if (n==undefined && k==undefined) {
+			[n,k] = line.split(' ')
+			if (n==undefined || k==undefined) {
+				throw new Error("인풋 받는거 잘못됨")
+			}
+		} else {
+			spaces.push(line.split(' '))
+			if (spaces.length >= n) {
+				rl.close();
+				break
+			}
+		}
+	}
+	
+	solution(n, k, spaces)
+	
+	process.exit();
 })();
 
 function solution(n, k, spaces) {
-  const arr = new DynamicArray();
-
-  for (var i = 0; i < spaces.length; i++) {
-    addSpecies(arr, spaces[i]);
-		// console.log(arr.entries())
-  }
-
-  console.log(countCompeteAreaSize(arr, k));
+	// brute force -> 1,000,000칸에 올라간 종의 수 다 세기
+	const arr = Array.from({length: metaverseSize}, () => Array.from({length: metaverseSize}, () => 0));
+	
+	for (var i = 0; i < spaces.length ; i++) {
+		addSpecies(arr, spaces[i])
+	}
+	
+	console.log(countCompeteAreaSize(arr, k))
+	
 }
 
 function addSpecies(arr, space) {
-  if (space.length != 4) return;
-  const [minX, minY, maxX, maxY] = space;
-  for (var i = minX; i < maxX; i++) {
-    for (var j = minY; j < maxY; j++) {
-      arr.increase(i, j);
-    }
-  }
+	if (space.length != 4) return
+	const [minX, minY, maxX, maxY] = space
+	for (var i = minX; i <= maxX; i++) {
+		for (var j = minY; j < maxY; j++) {
+			arr[j][i]++
+		}
+	}
+	console.log(arr.toMatrixString())
+	console.log('\n')
 }
 
 function countCompeteAreaSize(arr, k) {
-  return arr.count(k);
+	let count = 0
+	for (var i=0; i<arr.length; i++) {
+		for (var j=0; j<arr[0].length; j++) {
+			if (arr[j][i] == k) {count++}
+		}
+	}
+	return count
 }
 
-class DynamicArray {
-  constructor() {
-    this.data = new Map();
-  }
+// @todo delete me
+Array.prototype.toMatrixString = function () {
+	if (!Array.isArray(this) || !Array.isArray(this[0])) {
+			throw new TypeError('The array must be a 2D array.');
+	}
 
-  // 내부적으로 사용하는 키 생성 함수
-  _getKey(x, y) {
-    return `${x},${y}`;
-  }
-
-  _validate_index(x, y) {
-    return x >= 0 && x < METAVERSE_SIZE && y >= 0 && y < METAVERSE_SIZE;
-  }
-
-  // 값을 설정하는 함수
-  set(x, y, value) {
-		if (!this._validate_index(x,y)) { return; }
-    const key = this._getKey(x, y);
-    this.data.set(key, value);
-  }
-
-  // 값을 조회하는 함수
-  get(x, y) {
-		if (!this._validate_index(x,y)) { return; }
-    const key = this._getKey(x, y);
-    return this.data.has(key) ? this.data.get(key) : undefined;
-  }
-
-  increase(x, y, a = 1) {
-		if (!this._validate_index(x,y)) { return; }
-
-    const key = this._getKey(x, y);
-    if (this.get(x, y)) {
-      let old_value = this.get(x, y);
-      this.set(x, y, old_value + a);
-    } else {
-      this.set(x, y, a);
-    }
-  }
-
-  // 값을 삭제하는 함수
-  delete(x, y) {
-		if (!this._validate_index(x,y)) { return; }
-
-    const key = this._getKey(x, y);
-    return this.data.delete(key);
-  }
-
-  // 모든 데이터 조회
-  entries() {
-    const entries = [];
-    for (const [key, value] of this.data.entries()) {
-      const [x, y] = key.split(',').map(Number);
-      entries.push({ x, y, value });
-    }
-    return entries;
-  }
-
-  count(k) {
-    let k_count = 0;
-    for (const [key, value] of this.data.entries()) {
-      if (value == k) {
-        k_count++;
-      }
-    }
-    return k_count;
-  }
-}
+	return this.map(row => row.join(' ')).join('\n');
+};
